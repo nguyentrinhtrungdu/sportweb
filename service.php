6f2cf742b94489a67159a019bb9498f8452649ab
chodu <shenlol184@gmail.com>
2024-07-24 13:18:33 +0700

    service update

---
+++ service.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/fonts/fontawesome-free-6.5.2-web/css/all.min.css">
+    <link rel="stylesheet" href="./assets/css/service.css">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+        include ("./header.php");
+        ?>
+      <!-- workshop -->
+      <div class="workshop">
+           <div class="workshop-container"> 
+            <div class="workshop-tiltle">
+                <h1>
+                <span class="line-1">DỊCH VỤ IN ẤN</span><br>
+                <span class="line-2">BALO - TÚI HỘP - QUẦN ÁO  </span>
+            </h1>
+            </div>
+            <div class="workshop-container"> 
+                <div class="workshop-img">
+                    <img src="./assets/img/service/check_list_to_do-23_01c94ca4c13a495cb6d453605ecd8330.webp" alt="Slide 1">
+                </div>
+            </div>
+          
+        </div>
+  
+        </div>
+     <!-- quy cach -->
+      <div class="rule">
+        <div class="rule-container">
+            <h1 class="rule-heading">QUY CÁCH IN</h1>
+            <p class="rule-text">Khách hàng vui lòng ghi rõ nội dung in: viết thường hay in hoa chữ cái nào.</p>
+            <p class="rule-text">ĐỐI VỚI BALO- TÚI HỘP</p>
+            <p class="rule-text">In tên, số, logo CLB hoặc logo đội </p>
+            <p class="rule-text">Logo CLB có sẵn: MU, MC, CHELSEA, ARSENAL, LIVERPOOL, REAL MADRID, BARCELONA, PSG, JUVE, BAYERN MUNICH</p>
+            <p class="rule-text">Logo đội áp dụng cho đơn 5 túi hộp - balo trở lên</p>
+            <p class="rule-text">ĐỐI VỚI BỘ QUẦN ÁO KAMITO - ÁO BÓNG ĐÁ CHÍNH HÃNG</p>
+            <p class="rule-text">In tên, số (áo)</p>
+            <p class="rule-text">In số (quần)</p>
+        </div>
+        <div class="rule-imp-wrap">
+        <img src="/Lap trinh web/assets/img/service/Messi-an-mung2.jpg" alt="" class="rule-img">
+        <img src="/Lap trinh web/assets/img/service/Messi-an-mung2.jpg" alt="" class="rule-img">
+        <img src="/Lap trinh web/assets/img/service/Messi-an-mung2.jpg" alt="" class="rule-img">
+        <img src="/Lap trinh web/assets/img/service/Messi-an-mung2.jpg" alt="" class="rule-img">
+        </div>
+    </div>
+    <div class="rule2">
+        <div class="rule-im">
+            <img src="/Lap trinh web/assets/img/service/check_list_to_do-1-24_2cc250ad3eef4cc3af9e617f31c7358e_master.jpg" alt="" class="img-extra">
+            <img src="/Lap trinh web/assets/img/service/Adult-Kid-Soccer-Jersey-Customize-Football-Uniforms-Shirts-Men-Women-Futsal-Sportswear-Training-Tracksuit-Sports-Outfit.webp" alt="" class="img-extra">
+        </div>
+    </div>
+    <!-- phi in -->
+    <h1 class="header-text">PHÍ IN & CHIẾT KHẤU</h1>
+     <div class="phi-in">
+
+        
+        
+   <div class="left-table">
+    <div class="title">BẢNG GIÁ IN ẤN CÁC SẢN PHẨM KHÁC</div>
+    <table class="pricing-table">
+        <thead>
+            <tr>
+                <th>SẢN PHẨM</th>
+                <th>GIÁ</th>
+                <th>MÔ TẢ<br>(tối đa 3 chi tiết)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="highlight">
+                <td>BALO</td>
+                <td>30.000 Đ</td>
+                <td>TÊN + SỐ + LOGO CLB *</td>
+            </tr>
+            <tr class="highlight">
+                <td>TÚI HỘP</td>
+                <td>30.000 Đ</td>
+                <td>TÊN + SỐ + LOGO CLB *</td>
+            </tr>
+            <tr>
+                <td>BOOT ID</td>
+                <td>50.000 Đ</td>
+                <td>IN TÊN + SỐ + KÝ TỰ ĐẶC BIỆT (logo CLB, cờ..)</td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="note">* (logo đội áp dụng cho đơn hàng từ 5 cái trở lên)</div>
+    <table class="discount-table">
+        <thead>
+            <tr>
+                <th>SỐ LƯỢNG</th>
+                <th>CHIẾT KHẤU TRÊN TỔNG ĐƠN HÀNG</th>
+                <th>SỐ LƯỢNG</th>
+                <th>CHIẾT KHẤU TRÊN TỔNG ĐƠN HÀNG</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Dưới 5 cái</td>
+                <td>0%</td>
+                <td>20 - dưới 30 cái</td>
+                <td>20%</td>
+            </tr>
+            <tr>
+                <td>5 - dưới 10 cái</td>
+                <td>5%</td>
+                <td>Từ 30 cái trở lên</td>
+                <td>30%</td>
+            </tr>
+            <tr>
+                <td>10 - dưới 20 cái</td>
+                <td>10%</td>
+                <td colspan="2">Số lượng nhiều hơn: Liên hệ</td>
+                
+            </tr>
+        </tbody>
+    </table>
+    </div>
+    <div class="right-table">
+    <div class="title">BẢNG GIÁ IN ẤN QUẦN ÁO BÓNG ĐÁ</div>
+    <table class="pricing-table">
+        <thead>
+            <tr>
+                <th>SỐ LƯỢNG</th>
+                <th>Dưới 10 bộ</th>
+                <th>10 - dưới 20 bộ</th>
+                <th>20 - dưới 30 bộ</th>
+                <th>Từ 30 bộ trở lên</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="highlight">
+                <td>IN TÊN ÁO</td>
+                <td>20.000 Đ</td>
+                <td>20.000 Đ</td>
+                <td>20.000 Đ</td>
+                <td>20.000 Đ</td>
+            </tr>
+            <tr class="highlight">
+                <td>IN SỐ ÁO</td>
+                <td>30.000 Đ</td>
+                <td>30.000 Đ</td>
+                <td>30.000 Đ</td>
+                <td>30.000 Đ</td>
+            </tr>
+            <tr class="highlight">
+                <td>IN SỐ QUẦN</td>
+                <td>20.000 Đ</td>
+                <td>20.000 Đ</td>
+                <td>20.000 Đ</td>
+                <td>20.000 Đ</td>
+            </tr>
+            <tr>
+                <td>CÁC CHI TIẾT KHÁC<br>(LOGO, NHÀ TÀI TRỢ,..)</td>
+                <td>Không nhận in logo theo thiết kế</td>
+                <td>20.000 Đ</td>
+                <td>20.000 Đ</td>
+                <td>20.000 Đ</td>
+            </tr>
+            <tr>
+                <td>CHIẾT KHẤU TRÊN TỔNG ĐƠN HÀNG</td>
+                <td>0%</td>
+                <td>10%</td>
+                <td>15%</td>
+                <td>20%</td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="note">© 2024 In Ấn Quần Áo Bóng Đá. All rights reserved.</div>
+     </div>
+    </div>
+        <!-- footer -->
+        <?php
+        include ("./footer.php");
+        ?>
+       
+    </div>
+    <!--Modal layout-->
+    <?php
+        include ("./modal.php");
+        ?>
+   
+    
+</body>
+</html>
