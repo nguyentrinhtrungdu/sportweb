641a08b3bbca8e3ea32a55642ca348fbaaaf4f0f
chodu <shenlol184@gmail.com>
2024-07-24 18:47:32 +0700

    update repair shoes

---
+++ repairShoes.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/fonts/fontawesome-free-6.5.2-web/css/all.min.css">
+    <link rel="stylesheet" href="./assets/css/repairShoes.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <?php
+        include ("./header.php");
+        ?>
+    <!-- title  -->
+    <div class="title">
+        <div class="title-container">
+        <h1 class="title_text">DỊCH VỤ SỬA CHỮA</h1>
+        <h1 class="title_text"> GIÀY ĐÁ BANH CHÍNH HÃNG</h1>
+        </div>
+    </div>
+    <!-- ÉP KEO + MAY VẾT HỞ -->
+    <div class="container">
+        <div class="text-container">
+            <h1 class="text">ÉP KEO + MAY VẾT HỞ</h1>
+            <p class="text">Khi đôi giày gặp các vết hở ở vị trí hay ma sát, vết hở KHÔNG QUÁ 5CM VÀ GIÀY CHƯA BỊ MẤT FORM , chúng tôi sẽ áp dụng phương pháp ép keo + may vết hở để khắc phục lỗi và đảm bảo chắc chắn không bị hở lại.</p>
+        
+            <p class="text">Phí: 30.000Đ - Thời gian sửa chữa: 10-20 ngày</p>
+            <p class="text">Các trường hợp hay gặp lỗi này: 2 bên má trong hoặc má ngoài hay sút nhiều hoặc ma sát với mặt cỏ.</p>
+
+        </div>
+        <div class="image-contaner">
+            <img  src="https://file.hstatic.net/1000061481/file/untitled-1-01-02_ea71d3f0e8f54010ae5768c2ba12c872.jpg"lt="Image"class="img-content">
+        </div>
+    </div>
+     <!-- ÉP KEO + MAY 50% -->
+     <div class="container">
+        <div class="text-container">
+            <h1 class="text">ÉP KEO + MAY 50%</h1>
+            <p class="text">Khi đôi giày gặp các trường hợp hở keo phần mũi hoặc đệm gót (vị trí thường xuyên ma sát nhiều), chúng tôi sẽ sử dụng phương pháp ép keo + may 50% để khắc phục lỗi và đảm bảo chắc chắn không bị hở lại.</p>
+        
+            <p class="text">Phí: 40.000Đ/chiếc - Thời gian sửa chữa: 10-20 ngày</p>
+            <p class="text">Các trường hợp hay gặp lỗi này: Phần mũi của các đôi giày da tự nhiên hoặc Phần đệm gót của các đôi giày có bộ đệm</p>
+
+        </div>
+        <div class="image-contaner">
+            <img  src="https://file.hstatic.net/1000061481/file/untitled-1-01-02_ea71d3f0e8f54010ae5768c2ba12c872.jpg"lt="Image"class="img-content">
+        </div>
+    </div> <!--ÉP KEO + MAY TOÀN BỘ-->
+    <div class="container">
+        <div class="text-container">
+            <h1 class="text">ÉP KEO + MAY TOÀN BỘ</h1>
+            <p class="text">Các đôi giày bị hở keo nặng, vết hở lớn và bị hở nhiều vị trí, chúng tôi sẽ sử dụng phương pháp ép keo + may toàn bộ để giữ lại được form của đôi giày và đảm bảo chắc chắn đôi giày không bị hở lại.</p>
+        
+            <p class="text">Phí: 50.000Đ/chiếc - Thời gian sửa chữa: 10-20 ngày</p>
+            <p class="text">Các trường hợp hay gặp lỗi này: Các đôi giày sử dụng sau một thời gian dài keo sẽ tự khô hoặc va chạm, ma sát nhiều</p>
+
+        </div>
+        <div class="image-contaner">
+            <img  src="https://file.hstatic.net/1000061481/file/untitled-1-01-02_ea71d3f0e8f54010ae5768c2ba12c872.jpg"lt="Image"class="img-content">
+        </div>
+    </div> <!-- ĐẮP DA - VÁ CHỖ RÁCH -->
+    <div class="container">
+        <div class="text-container">
+            <h1 class="text">ĐẮP DA - VÁ CHỖ RÁCH</h1>
+            <p class="text">Các đôi giày bị lủng, rách da do va chạm hoặc upper bị mục nát do bảo quản không tốt, Chúng tôi sẽ sử dụng phương pháp đắp 1 lớp da lên trên và may vết rách. Đây cách duy nhất để cứu lại đôi giày, chỉ cố gắng làm chắc chắn để giữ lại form, KHÔNG ĐẶT NẶNG VẤN ĐỀ THẨM MỸ vì không có lớp da giống 100% của giày và cũng không thể đắp bên dưới upper vì không chắc chắn và sẽ gây cộm chân khi mang. </p>
+        
+            <p class="text">Phí: 60.000Đ/chiếc - Thời gian sửa chữa: 10-20 ngày</p>
+            <p class="text">Các trường hợp hay gặp lỗi này: Các đôi giày có upper mỏng, dễ rách hoặc upper bằng sợi vải dệt, thi đấu va chạm, ma sát nhiều hoặc bảo quản không tốt sẽ dễ mục rách</p>
+
+        </div>
+        <div class="image-contaner">
+            <img  src="https://file.hstatic.net/1000061481/file/untitled-1-01-02_ea71d3f0e8f54010ae5768c2ba12c872.jpg"lt="Image"class="img-content">
+        </div>
+    </div>
+
+    <!-- footer -->
+    <?php
+        include ("./footer.php");
+        ?>
+    </div>
+    <!--Modal layout-->
+    <?php
+        include ("./modal.php");
+        ?>
+      
+    <script src="banner.js"></script>
+</body>
+</html>
